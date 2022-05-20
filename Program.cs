3327f61d362770733475394b51b7eb18b06dacb1
gabrielmarcos2 <gabrielsqueda123@gmail.com>
2022-05-20 20:40:12 -0300

    tarefa finalizada

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Incio: ");
+int inicio = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("fim: ");
+int fim  = Convert.ToInt32(Console.ReadLine());
+
+if (fim >= inicio)
+{
+    int n = inicio;
+
+    while (n <= fim)
+    {
+        Console.Write($"{n} ");
+        n += 1;
+    }
+}
+else
+{
+    Console.WriteLine("Não é possivel terminar antes de iniciar ");
+}
